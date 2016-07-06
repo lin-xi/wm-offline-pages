0bdf978848e9741cdbe82616b491ee6257f29227
lin-xi <mcnjqe@163.com>
2016-07-06 14:22:30 +0800

    update

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -23,12 +23,19 @@ const IndexPage = React.createClass({
             pluginId: localStorage.getItem('pluginId') || '',
             pageName: localStorage.getItem('pageName') || '',
             title: localStorage.getItem('title') || '',
-            deployUrl: localStorage.getItem('deployUrl') || ''
+            url: localStorage.getItem('deployUrl') || ''
         };
     },
 
     getDebugUrl(){
-        return "bdwm://plugin?pluginId=" + this.state.pluginId + "&pageName="+ encodeURIComponent(this.state.pageName) +"&title=123";
+        var me = this;
+        var url = 'bdwm://plugin?pluginId={pluginId}&pageName={pageName}&title={title}&url={url}';
+        return url.replace(/\{(.*?)\}/g, function(s0, s1){
+            if(s1 == 'title' || s1 == 'url'){
+                return encodeURIComponent(me.state[s1]);
+            }
+            return me.state[s1];
+        })
     },
 
     render() {
@@ -41,7 +48,7 @@ const IndexPage = React.createClass({
                         <TextField id="pluginId" onChange={this.handleChange} hintText="pluginId" floatingLabelText="pluginId" value={this.state.pluginId}/><br/>
                         <TextField id="pageName" onChange={this.handleChange} hintText="pageName" floatingLabelText="页面名称" value={this.state.pageName}/><br/>
                         <TextField id="title" onChange={this.handleChange} hintText="title" floatingLabelText="标题" value={this.state.title}/><br/>
-                        <TextField id="deployUrl" onChange={this.handleChange} hintText="deploy url" floatingLabelText="部署地址" value={this.state.deployUrl}/>
+                        <TextField id="url" onChange={this.handleChange} hintText="package url" floatingLabelText="包地址" value={this.state.url}/>
                     </span>
                     <span className="right">
                         <QRCode value={this.getDebugUrl()} size={256} level="M" bgColor="#fff"/>
@@ -58,7 +65,7 @@ const IndexPage = React.createClass({
                         <RaisedButton label="android" primary={true}/>
                     </span>
                     <span className="right">
-                        <img src=""/>
+                        <img src="http://172.17.138.208:8080/jenkins/job/WMapp_iOS/348/artifact/image.png"/>
                         <RaisedButton label="ios" primary={true}/>
                     </span>
                 </div>
@@ -83,7 +90,6 @@ const IndexPage = React.createClass({
                             <p className="l1">root: "./build"</p>
                             <p className="l1">deploy: "./build"</p>
                             <p className="l2">receiver: "",</p>
-                            <p className="l2">from: "",</p>
                             <p className="l2">to: "",</p>
                             <p className="l1">{"}"}</p>
                             <p>{"}"}</p>
